ac975d4395322805c49c5b5eceaac59bcc156330
lsiten <lsiten@Eric-mini.local>
2018-12-06 21:04:55 +0800

    debug

--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
+  <div class="user-index">
+    <blur :blur-amount=40 :url="userInfo.img">
+      <p class="center"><img :src="userInfo.img"></p>
+    </blur>
+    <group>
+      <cell :title="$t('components.user.account')" :value="$t('components.user.protected')" ></cell>
+      <cell :title="$t('components.user.money')"  :is-loading="!userInfo.money" :value="userInfo.money"></cell>
+    </group>
+
   </div>
 </template>
 <script>
+import {Blur, Group, Cell} from 'vux'
 export default {
   name: 'user-index',
+  data () {
+    return {
+      userInfo: {
+        img: 'http://wx3.sinaimg.cn/bmiddle/b432fab8gy1fi27yeop6cj20jg0jgjrr.jpg',
+        money: 20
+      }
+    }
+  },
   created () {
-    let authUrl = 'https://mixin.one/oauth/authorize?client_id=28fd840c-7cdf-4c1a-a8f0-4703b4cbec32&scope=PROFILE:READ+PHONE:READ+ASSETS:READ&code_challenge=PKCE'
+    let authUrl = 'https://mixin.one/oauth/authorize?client_id=8f3f85bd-1f78-4cbc-9081-fb9d7d7b8672&scope=PROFILE:READ+PHONE:READ+ASSETS:READ&code_challenge=PKCE'
     let code = this.$route.query.code
     if (code) {
       this.$store.dispatch('com_set_code', code)
@@ -16,6 +33,29 @@ export default {
       code = localStorage.getItem('mixin_code')
     }
     !code && (window.location.href = authUrl)
+  },
+  components: {
+    Blur,
+    Group,
+    Cell
   }
 }
 </script>
+
+<style lang="less" scoped>
+.user-index {
+  .center {
+    text-align: center;
+    padding-top: 20px;
+    color: #fff;
+    font-size: 18px;
+  }
+  .center img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 4px solid #ececec;
+  }
+}
+</style>
+
